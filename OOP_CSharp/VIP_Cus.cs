10bb4f02e091e1a609e12225d1df631e64253cf3
holovo912 <hoanglongvu912@gmail.com>
2022-12-08 11:06:55 +0700

    fixed

--- OOP_CSharp/VIP_Cus.cs
+++ OOP_CSharp/VIP_Cus.cs
@@ -1,23 +1,16 @@
-﻿namespace OOP_CSharp
+﻿using System.Collections.Generic;
+
+namespace OOP_CSharp
 {
     public class VIP_Cus : Customer
     {
+        public VIP_Cus() : base() { }
         public override void Input(string ID, string ten, int tuoi)
         {
             this.ID = ID;
             this.Ten = ten;
             this.Tuoi = tuoi;
             this.Giamgia = 0.2;
-        }
-        public static bool operator ==(Customer d1, VIP_Cus d2)
-        {
-            if (d1.ID == d2.ID) return true;
-            return false;
-        }
-        public static bool operator !=(Customer d1, VIP_Cus d2)
-        {
-            if (d1.ID == d2.ID) return false;
-            return true;
         }
         ~VIP_Cus() { }
     }
